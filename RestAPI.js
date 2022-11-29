f3541ea534b6c5d49af0a474e2efbf17f81d8a90
OddCompost <richomas@hotmail.com>
2022-11-29 15:49:07 +0000

    working

--- RestAPI.js
+++ RestAPI.js
@@ -1,7 +1,11 @@
 //Create Rest API for the application
 const express = require('express');
 const {cors, auth} = require('./middleware');
-const {sequelize, Message} = require('./modles')
+const {sequelize, Message, User} = require('./modles')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config(); //Loads env file into enviroment 
 
 //Test you have connected to the database
 
@@ -10,17 +14,85 @@ let app = express();
 let port = process.env.PORT || 3000;
 
 //Check the server status and listen to the port
-app.use(cors, auth, express.json()); //!Tells express we are expecting JSON data & to use cors
+app.set("json spaces", 2);
+app.use(cors, auth, express.json()); //!Tells express we are expecting JSON data & to use cors "This is middleware" 
 app.listen(port, async function() {
-    await sequelize.sync();
-    console.log('Server is running on port: ' + port);
+    try{
+         await sequelize.sync();
+         console.log('Server is running on port: ' + port);
+    }
+    catch (err){
+        console.error('Failed to start server ', err);
+    }
+    
+    
+});
+
+//?Log user in 
+
+app.post('/register', async function(req, res){
+    const {username, password} = req.body;
+    try {
+        //1. Check username against db
+        const user = await User.findOne({
+            where: {
+                userName: username
+            }
+        });
+        
+        //If user exists do something
+        if(user){
+            res.status(401).send('User already exists');
+            return
+        }
+        
+        //Set up new user
+        const hashedPW = await bcrypt.hash(password, Number.parseInt(process.env.SALT_COUNT)); //Hash PW
+        const {id, username: createdUserName} = await User.create({password: hashedPW, userName: username});
+        //Create token by parsing in the user obj as well as the secret to generate the token => send to user client
+        const token = jwt.sign({id, createdUserName}, process.env.SECRETOKEN);
+        res.send({message: 'Token has been sent, Dont lose it!', token});
+    } 
+        catch (error) {
+            res.status(401).send({Message: 'There was an error',  error});
+            console.log(error);
+        }
+});
+
+app.post('/login', async (req, res, next) =>{
+    const {username, password} = req.body;
+    try {
+        //1. Check username against db
+        const user = await User.findOne({
+            where: {
+                userName: username
+            }
+        });
+        
+        //If user exists check password hash and log them in
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        console.log(passwordMatch);
+        const token = jwt.sign({id : user.id, username : user.userName}, process.env.SECRETOKEN);
+        if(passwordMatch){
+            res.status(201).send({token}); //Send token to user if the password hash matches - Keeps user logged in
+        }
+        else{
+            res.status(401).send('No match GTFO!');
+        }
+
+    } 
+        catch (error) {
+            res.status(401).send({Message: 'There was an error',  error});
+            console.log(error);
+        }
 
 });
 
 //Get all request
 app.get('/messages', async function(req, res) {
+    console.log(req.user);
     try {
-        let message = await  Message.findAll();
+        let message = await  Message.findAll({where: {id: req.user.id}});
         res.status(201).send(message);
     } catch (error) {
         if(error instanceof ValidationError){
--- middleware.js
+++ middleware.js
@@ -12,75 +12,32 @@
 const http = require('http');
 const bcrypt = require('bcryptjs');
 const {User} = require('./modles');
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); //Loads env file into enviroment 
 
-function cors(rec,res,next){
+
+function cors(req,res,next){
       res.set('ACCESS-CONTROL-ALLOW-ORIGIN', '*');
       next();
 }
 
 /**
  * 
- * @param {Request} rec 
+ * @param {Request} req 
  * @param {Response} res 
  * @param {Next} next 
  * @returns 
  */
-async function auth(rec,res,next){
-      //1. Check for auth header 
-      let header = rec.get('Authorization');
-      //Intiates browser popup for authenticatiion
-      //If the browser did not send us a auth header send 401 - set it if it does not exist 
+function auth(req,res,next){
+      let  header = req.get("Authorization");
       if(!header){
-            res.set('WWW-AUTHENTICATE', 'Basic'); 
-            res.status(401).send({"Error" : http.STATUS_CODES[401]});
-            return;
-      }
-      //2. Check auth type and token 
-      //Gets the base 64encoded string and splits it "Basic klnsdcknsdkj"
-      let [type, token] = header.split(' ');
-      if(type != 'Basic'|| !token ){
-            res.status(401).send({"Error" : http.STATUS_CODES [401]});
+            next();
             return
       }
-
-      //3. Decode user name and password 
-            let buffer = Buffer.from(token, 'base64');
-            let decoded = buffer.toString('utf-8');
-
-            //Username:Password
-            let [userName, password] = decoded.split(':');
-
-            if(!password){
-                  res.status(401).send({"Error" : http.STATUS_CODES [401]});
-                  return;
-            }
-
-      //4. Find user in db 
-
-            try {
-                  let user = await User.findOne({
-                        where: {
-                              userName: userName
-                        }
-                  });
-
-                  //Checks password even if none exists so that the timing is the same allways 
-                  let hash = user? user.get('password') : 'No match';
-
-                  //Compare hash against password (True = sucess)
-                  let success = await bcrypt.compare(password, hash);
-
-                  if(success){
-                        next();
-                        return;
-                  }
-                  res.status(401).send({"Error" : http.STATUS_CODES [401]});
-
-            } catch (error) {
-                  res.status(500).send({"Error" : http.STATUS_CODES [500]});
-                  next(error);
-            }
-
+      let [type, token] = header.split(" "); //Token
+      const user = jwt.verify(token, process.env.SECRETOKEN);
+      req.user = user;
+      next();
 }
 
 module.exports = {cors, auth};
